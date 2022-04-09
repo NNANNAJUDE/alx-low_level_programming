02dd3d2673eadad81c682328874722d862c7eef9
Nnanna Jude <judegr8@yahoo.com>
2022-04-09 11:45:04 -0700

    compilation corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.stdio.h>
+#include <stdio.h>
 /**
  * main - first function to output c programming and
  * Return: 0 if successful
  */
 int main(void)
 {
-	putputs("\"Programming is like building a multilingual puzzle"
-);
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return  (0);
 }
