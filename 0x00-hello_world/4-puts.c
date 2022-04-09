c69f3dae4e03459f2b57c7b8673a0119c5c67a71
Nnanna Jude <judegr8@yahoo.com>
2022-04-09 10:39:19 -0700

    my c function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.stdio.h>
+/**
+ * main - first function to output c programming and
+ * Return: 0 if successful
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"
+);
+return (0);
+}
