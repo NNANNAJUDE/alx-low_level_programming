fdd308860204708e962b79d0dfa10f2198cea21d
Nnanna Jude <judegr8@yahoo.com>
2022-04-11 02:44:26 -0700

    correct

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include  <stdio.h>
+/**
+ * main - function to print lowercase except q and a
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
