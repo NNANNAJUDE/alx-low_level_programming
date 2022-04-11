b07d76b806f1c89f7312dde2f77342eded0bbb44
Nnanna Jude <judegr8@yahoo.com>
2022-04-11 03:00:32 -0700

    correct

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function to print numbernumbers to base starting from 0
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
