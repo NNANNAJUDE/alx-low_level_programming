0037f1836a0bb647ea9ca83922ae872e4ef4d43f
Nnanna Jude <judegr8@yahoo.com>
2022-04-11 03:13:40 -0700

    correct

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function to print numbersbase 10 numbers
+ * starting from 0
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar("\n");
+	return (0);
+}
