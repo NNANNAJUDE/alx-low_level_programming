243be3d7f6c46bdd1e96c5787ce9e2fba852dca8
Nnanna Jude <judegr8@yahoo.com>
2022-04-13 21:04:16 -0700

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_×10 - function to print alphabet
+ * Return: 0 on success
+ */
+
+void print_alphabet_×10(void)
+{
+	int alphabet;
+	int count;
+
+
+	count = 0;
+	while (count < 10)
+{
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	count++;
+	_putchar('\n');
+}
+
+}
