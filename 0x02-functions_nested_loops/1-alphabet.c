8d92fdc950292513fa0949d1ae26ac042268e05f
Nnanna Jude <judegr8@yahoo.com>
2022-04-13 20:41:49 -0700

    check

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print alphabet
+ * Return: 0 on successful
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+
+}
