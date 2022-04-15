b0c14676c0b4a3071cca82336cfc497df9278a2e
Nnanna Jude <judegr8@yahoo.com>
2022-04-15 12:09:22 -0700

    check

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+
 /**
  * main - header file for prototypes
  * void - void prototypes
  * int - prototypes
  */
+
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
